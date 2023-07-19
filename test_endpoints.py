e0f74492de31386e0b8391ce7ce4912c4641005b
Andy Tillo <andy.tillo@gmail.com>
2023-07-19 10:27:33 -0700

    clean JSON and inc values

--- test_endpoints.py
+++ test_endpoints.py
@@ -25,9 +25,18 @@ def create_session(username, password):
         return None, None
     return session, response.json()['session_id']
 
+def count_endpoints(endpoints):
+    count = 0
+    for category, request_types in endpoints.items():
+        for request_type, endpoint_data_list in request_types.items():
+            count += len(endpoint_data_list)
+    return count
+
 def load_endpoints():
     with open('endpoints.json') as f:
-        return json.load(f)
+        endpoints = json.load(f)
+        print(f"Loaded {count_endpoints(endpoints)} endpoints.")
+        return endpoints
 
 def preprocess_endpoints(endpoints):
     ready_to_test = {}
@@ -78,10 +87,30 @@ def test_endpoints(session, user_id, ready_to_test, parameters_needed):
                 if missing_parameters:  # If there are still some missing params, print 'INCOMPLETE' and move to the next endpoint
                     print(f"\nEndpoint: {endpoint} {Fore.YELLOW}INCOMPLETE{Fore.RESET}")
                     incomplete_count += 1
+                    endpoint_entry = {
+                        "endpoint": endpoint,
+                        "test_result": "Incomplete",
+                        "failure_details": None,
+                        "test_date": time.time()
+                    }
+                    test_result_data.setdefault(category, {}).setdefault(request_type, []).append(endpoint_entry)
                     continue
 
-                url = base_url + endpoint
-                response = session.request(request_type, url)
+                headers = {}
+                params = {}
+                if 'Peloton-Platform' in endpoint:
+                    headers['Peloton-Platform'] = "your-value"
+
+                if 'user_query' in endpoint:
+                    params['user_query'] = "your-value"
+
+                try:
+                    url = base_url + endpoint
+                    response = session.request(request_type, url, headers=headers, params=params)
+                except Exception as e:
+                    print(f"\nRequest failed due to {e}")
+                    continue
+
                 print(f"\nEndpoint: {endpoint}")
                 print(f'Status Code: {response.status_code}', end=' ')
 
@@ -90,11 +119,11 @@ def test_endpoints(session, user_id, ready_to_test, parameters_needed):
                     print(f'{Fore.GREEN}SUCCESS!{Fore.RESET}')
                     success_count += 1
                     endpoint_result = 'Successful'
+
                 else:
                     print(f'{Fore.RED}FAIL{Fore.RESET}')
                     fail_count += 1
                     endpoint_result = 'Failed'
-                    # ... Rest of your code ...
 
                     try:
                         if response.headers['content-type'] == 'application/json':
@@ -117,13 +146,12 @@ def test_endpoints(session, user_id, ready_to_test, parameters_needed):
                             "message": 'Error during response parsing'
                         }
 
-                # update test results in ready_to_test
                 endpoint_entry = {
-                    "endpoint": endpoint,
-                    "test_result": endpoint_result,
-                    "failure_details": failure_details,
-                    "test_date": time.time()
-                }
+                        "endpoint": endpoint,
+                        "test_result": endpoint_result,
+                        "failure_details": failure_details,
+                        "test_date": time.time()
+                    }
                 test_result_data.setdefault(category, {}).setdefault(request_type, []).append(endpoint_entry)
 
                 if VERBOSE_MODE:
@@ -142,14 +170,38 @@ def test_endpoints(session, user_id, ready_to_test, parameters_needed):
     print(f"Failed calls: {Fore.RED}{fail_count}{Fore.RESET}")
     print(f"Incomplete calls: {Fore.YELLOW}{incomplete_count}{Fore.RESET}")
 
+    # include the check here as well right before writing the file
+    print(f"Going to save {count_endpoints(endpoints)} endpoints. If no errors were shown above, the file will be overwritten.")
+
+    # write to a temporary file first
+    with open('endpoints_temp.json', 'w') as f:
+        json.dump(endpoints, f, indent=4)
+
+    # if everything is okay until now, replace the original file
+    os.rename('endpoints_temp.json', 'endpoints.json')
+
+
 def main():
     session, user_id = create_session(username, password)
     if session is None:
         print("Login unsuccessful. Exiting...")
         return
+    
     endpoints = load_endpoints()
+    initial_count = count_endpoints(endpoints)
     ready_to_test, parameters_needed = preprocess_endpoints(endpoints)
+
+    global test_result_data
+    test_result_data = {}
+
     test_endpoints(session, user_id, ready_to_test, parameters_needed)
+
+    final_count = count_endpoints(endpoints)
+
+    if initial_count != final_count:
+        print(f"WARNING: Initial count of endpoints ({initial_count}) does not match final count ({final_count}).")
+    else:
+        print(f"Endpoint counts matched. Initial and final count is {initial_count}.")
 
 if __name__ == "__main__":
     main()
